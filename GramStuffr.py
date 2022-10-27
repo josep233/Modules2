8ac2ff1c4c3c2b35be75385768ae026e6d7c4742
Joseph <joeyscarter@gmail.com>
2022-10-27 16:27:32 -0600

    N/A

--- GramStuffr.py
+++ GramStuffr.py
@@ -161,34 +161,34 @@ class Test_assembleForceVector( unittest.TestCase ):
         gold_force_vector = numpy.array( [ 1.0/30.0, 1.0/10.0, 1.0/5.0 ] )
         self.assertTrue( numpy.allclose( test_force_vector, gold_force_vector ) )
 #=============================================================================================================================================
-class Test_computeGalerkinApproximation( unittest.TestCase ):
-    def test_cubic_polynomial_target( self ):
-        # print( "POLY TEST" )
-        target_fun = lambda x: x**3 - (8/5)*x**2 + (3/5)*x
-        domain = [ 0, 1 ]
-        degree = 2
-        solution_basis = Basis.evalBernsteinBasis1D
-        test_sol_coeff = computeGalerkinApproximation(target_fun,domain,degree,solution_basis)
-        gold_sol_coeff = numpy.array( [ 1.0 / 20.0, 1.0 / 20.0, -1.0 / 20.0 ] )
-        fit_err = computeFitError( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
-        # plotCompareGoldTestSolution( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
-        # plotCompareFunToTestSolution( target_fun, test_sol_coeff, domain, solution_basis )
-        self.assertTrue( numpy.allclose( gold_sol_coeff, test_sol_coeff ) )
-        self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-12 )
+# class Test_computeGalerkinApproximation( unittest.TestCase ):
+#     def test_cubic_polynomial_target( self ):
+#         print( "POLY TEST" )
+#         target_fun = lambda x: x**3 - (8/5)*x**2 + (3/5)*x
+#         domain = [ 0, 1 ]
+#         degree = 2
+#         solution_basis = Basis.evalBernsteinBasis1D
+#         test_sol_coeff = computeGalerkinApproximation(target_fun,domain,degree,solution_basis)
+#         gold_sol_coeff = numpy.array( [ 1.0 / 20.0, 1.0 / 20.0, -1.0 / 20.0 ] )
+#         fit_err = computeFitError( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
+#         # plotCompareGoldTestSolution( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
+#         plotCompareFunToTestSolution( target_fun, test_sol_coeff, domain, solution_basis )
+#         self.assertTrue( numpy.allclose( gold_sol_coeff, test_sol_coeff ) )
+#         self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-12 )
 
-    def test_sin_target( self ):
-        # print( "SIN TEST" )
-        target_fun = lambda x: numpy.sin( numpy.pi * x )
-        domain = [ 0, 1 ]
-        degree = 2
-        solution_basis = Basis.evalBernsteinBasis1D
-        test_sol_coeff = computeGalerkinApproximation(target_fun,domain,degree,solution_basis)
-        gold_sol_coeff = numpy.array( [ (12*(numpy.pi**2 - 10))/(numpy.pi**3), -(6*(3*numpy.pi**2 - 40))/(numpy.pi**3), (12*(numpy.pi**2 - 10))/(numpy.pi**3)] )
-        fit_err = computeFitError( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
-        print(fit_err)
-        # plotCompareGoldTestSolution( gold_sol_coeff, test_sol_coeff, [0, 1], solution_basis )
-        # plotCompareFunToTestSolution( target_fun, test_sol_coeff, domain, solution_basis )
-        self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-1 )
+    # def test_sin_target( self ):
+    #     # print( "SIN TEST" )
+    #     target_fun = lambda x: numpy.sin( numpy.pi * x )
+    #     domain = [ 0, 1 ]
+    #     degree = 2
+    #     solution_basis = Basis.evalBernsteinBasis1D
+    #     test_sol_coeff = computeGalerkinApproximation(target_fun,domain,degree,solution_basis)
+    #     gold_sol_coeff = numpy.array( [ (12*(numpy.pi**2 - 10))/(numpy.pi**3), -(6*(3*numpy.pi**2 - 40))/(numpy.pi**3), (12*(numpy.pi**2 - 10))/(numpy.pi**3)] )
+    #     fit_err = computeFitError( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
+    #     print(fit_err)
+    #     # plotCompareGoldTestSolution( gold_sol_coeff, test_sol_coeff, [0, 1], solution_basis )
+    #     plotCompareFunToTestSolution( target_fun, test_sol_coeff, domain, solution_basis )
+    #     self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-1 )
         
     # def test_erfc_target( self ):
     #     # print( "ERFC TEST" )
@@ -200,18 +200,18 @@ class Test_computeGalerkinApproximation( unittest.TestCase ):
     #     gold_sol_coeff = numpy.array( [ 1.8962208131568558391841630949727, 2.6917062016799657617278998883219, -0.69170620167996576172789988832194, 0.10377918684314416081583690502732] )
     #     fit_err = computeFitError( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
     #     # plotCompareGoldTestSolution( gold_sol_coeff, test_sol_coeff, [-2, 2], solution_basis )
-    #     # plotCompareFunToTestSolution( target_fun, test_sol_coeff, domain, solution_basis )
-    #     self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-4 )
+    #     plotCompareFunToTestSolution( target_fun, test_sol_coeff, domain, solution_basis )
+    #     self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-0 )
     
-    # def test_exptx_target( self ):
-    #     # print( "EXPT TEST" )
-    #     target_fun = lambda x: float( numpy.real( float( x )**float( x ) ) )
-    #     domain = [ -1, 1 ]
-    #     degree = 5
-    #     solution_basis = Basis.evalBernsteinBasis1D
-    #     test_sol_coeff = computeGalerkinApproximation( target_fun = target_fun, domain = domain, degree = degree, solution_basis = solution_basis )
-    #     gold_sol_coeff = ( [ -0.74841381974620419634327921170757, -3.4222814978197825394922980704166, 7.1463655364038831935841354617843, -2.9824200396151998304868767455064, 1.6115460899636204992283970407553, 0.87876479932866366847320748048494 ] )
-    #     fit_err = computeFitError( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
-    #     # plotCompareGoldTestSolution( gold_sol_coeff, test_sol_coeff, [-1, 1], solution_basis )
-    #     # plotCompareFunToTestSolution( target_fun, test_sol_coeff, domain, solution_basis )
-    #     self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-2 )
+    def test_exptx_target( self ):
+        # print( "EXPT TEST" )
+        target_fun = lambda x: float( numpy.real( float( x )**float( x ) ) )
+        domain = [ -1, 1 ]
+        degree = 5
+        solution_basis = Basis.evalBernsteinBasis1D
+        test_sol_coeff = computeGalerkinApproximation( target_fun = target_fun, domain = domain, degree = degree, solution_basis = solution_basis )
+        gold_sol_coeff = ( [ -0.74841381974620419634327921170757, -3.4222814978197825394922980704166, 7.1463655364038831935841354617843, -2.9824200396151998304868767455064, 1.6115460899636204992283970407553, 0.87876479932866366847320748048494 ] )
+        fit_err = computeFitError( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
+        # plotCompareGoldTestSolution( gold_sol_coeff, test_sol_coeff, [-1, 1], solution_basis )
+        plotCompareFunToTestSolution( target_fun, test_sol_coeff, domain, solution_basis )
+        self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-2 )
